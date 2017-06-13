b0104852f731ba1b5c4e689404d0754e3d8b896b
fabricio-cardoso <fabriciocardosom@gmail.com>
2017-06-13 20:38:10 -0300

    concluido o metodo de retornaMaior

--- src/main/java/RetornaMaior.java
+++ src/main/java/RetornaMaior.java
@@ -1,13 +1,29 @@
+import java.util.Scanner;
+
 
 public class RetornaMaior {
 
 	/**
 	 * @param args
+	 * @return 
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("testando");
-		System.out.println("Retornando maior");
+		Scanner in = new Scanner (System.in);
+		int num1, num2;
+		System.out.println("Digite um numero: ");
+		num1 = in.nextInt();
+		System.out.println("Digite outro numero: ");
+		num2 = in.nextInt();
+		
+		System.out.println("O maior numero é: "+maior(num1, num2));
+	}
+	public static int maior(int a,int b){
+		if(a > b){
+			return a;
+		}else{
+			return b;
+		}
 	}
 
 }
